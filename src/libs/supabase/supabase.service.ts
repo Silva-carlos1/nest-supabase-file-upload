889165afd27d7ef509b962f47589a667f9ba57f2
Carlos <silva.carloseduardo.ds@gmail.com>
2024-07-07 20:23:37 -0300

    feat: add Supabase module

---
+++ src/libs/supabase/supabase.service.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseProvider } from './supabase.provider';
+
+type UploadFle = {
+  contentType: 'audio/mpeg';
+  bucketName: string;
+  fileName: string;
+  file: Buffer;
+};
+
+@Injectable()
+export class SupabaseService {
+  constructor(
+    @Inject(SupabaseProvider.provide)
+    private readonly supabaseProvider: SupabaseClient<any, 'public', any>,
+  ) {}
+
+  async uploadFile({ bucketName, contentType, file, fileName }: UploadFle) {
+    const data = await this.supabaseProvider.storage
+      .from(bucketName)
+      .upload(fileName, file, { upsert: true, contentType });
+
+    return data;
+  }
+}
